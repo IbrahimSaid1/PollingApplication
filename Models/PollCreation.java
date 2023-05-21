4716845953139970f3c35023996ba4a61173277c
IbrahimSaid1 <squ118977@gmail.com>
2023-05-21 16:44:33 +0400

    Update PollCreation class

--- Models/PollCreation.java
+++ Models/PollCreation.java
@@ -1,4 +1,26 @@
 package com.codeline.PollingApplication.Models;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Data
+@Entity
 public class PollCreation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long Id;
+    String question;
+    String choices;
+
 }
