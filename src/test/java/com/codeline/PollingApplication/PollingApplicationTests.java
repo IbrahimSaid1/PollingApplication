7ff921518147704ae1cef8e10955d308538df101
IbrahimSaid1 <squ118977@gmail.com>
2023-05-21 11:36:48 +0400

    Add Packages and Register Class

---
+++ src/test/java/com/codeline/PollingApplication/PollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.PollingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
