4f4e411805f4d1efec424a7627bcd976224e9a3e
IbrahimSaid1 <squ118977@gmail.com>
2023-05-21 14:32:09 +0400

    Update RegisterService Class

--- Service/RegisterService.java
+++ Service/RegisterService.java
@@ -5,4 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+public class RegisterService {
+
+    @Autowired
+    RegisterRepository registerRepository;
+
+    public void saveRegister(Register register){
+        registerRepository.save(register);
+    }
+}
+
 
