74a88ddf063474f60cfed66342edf965eca74cc9
IbrahimSaid1 <squ118977@gmail.com>
2023-05-21 16:54:01 +0400

    RequestMapping for PollCreationController

--- Controller/PollCreationController.java
+++ Controller/PollCreationController.java
@@ -1,4 +1,20 @@
 package com.codeline.PollingApplication.Controller;
 
+import com.codeline.PollingApplication.RequestObjects.GetPollCreationRequestObject;
+import com.codeline.PollingApplication.Service.PollCreationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PollCreationController {
+
+    @Autowired
+    PollCreationService pollCreationService;
+
+    @RequestMapping("/api/pollcreation")
+    public void savePollCreation(@RequestBody GetPollCreationRequestObject pollCreationRequestObject){
+        createPollCreation(pollCreationRequestObject);
+    }
 }
