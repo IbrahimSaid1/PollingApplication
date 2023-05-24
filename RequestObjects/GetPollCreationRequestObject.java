358f9fe60e90dfe9725ab626f0520f704babb5ed
IbrahimSaid1 <squ118977@gmail.com>
2023-05-24 11:28:57 +0400

    Update GetPollCreationRequestObject

--- RequestObjects/GetPollCreationRequestObject.java
+++ RequestObjects/GetPollCreationRequestObject.java
@@ -1,4 +1,18 @@
 package com.codeline.PollingApplication.RequestObjects;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class GetPollCreationRequestObject {
+
+    String question;
+    String choices;
+
 }
