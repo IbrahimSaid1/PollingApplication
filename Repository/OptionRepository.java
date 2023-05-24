f58bd0ec619804581eb30cea70869ae024baf7ae
IbrahimSaid1 <squ118977@gmail.com>
2023-05-24 16:53:48 +0400

    Update OptionRepository and PollVoteRepository

--- Repository/OptionRepository.java
+++ Repository/OptionRepository.java
@@ -1,4 +1,8 @@
 package com.codeline.PollingApplication.Repository;
 
-public class OptionRepository {
+import com.codeline.PollingApplication.Models.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
 }
--- Repository/PollVoteRepository.java
+++ Repository/PollVoteRepository.java
@@ -1,4 +1,8 @@
 package com.codeline.PollingApplication.Repository;
 
-public class PollVoteRepository {
+import com.codeline.PollingApplication.Models.PollVote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PollVoteRepository extends JpaRepository<PollVote, Long> {
 }
