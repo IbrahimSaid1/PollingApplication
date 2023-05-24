a3b358dd6d037d4b86b0b32ead8b2cf6688a8620
IbrahimSaid1 <squ118977@gmail.com>
2023-05-24 11:25:50 +0400

    Update PollCreationRepository

--- Repository/PollCreationRepository.java
+++ Repository/PollCreationRepository.java
@@ -1,4 +1,10 @@
 package com.codeline.PollingApplication.Repository;
 
-public class PollCreationRepository {
+import com.codeline.PollingApplication.Models.PollCreation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PollCreationRepository extends JpaRepository<PollCreation, Long> {
+
+
 }
--- Service/PollCreationService.java
+++ Service/PollCreationService.java
@@ -1,4 +1,18 @@
 package com.codeline.PollingApplication.Service;
 
+import com.codeline.PollingApplication.Models.PollCreation;
+import com.codeline.PollingApplication.Models.Register;
+import com.codeline.PollingApplication.Repository.PollCreationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PollCreationService {
+
+    @Autowired
+    PollCreationRepository pollCreationRepository;
+    public void savePollCreation(PollCreation pollCreation){
+        pollCreationRepository.save(pollCreation);
+    }
+
 }
