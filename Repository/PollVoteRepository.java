a098fa29667ace63fae61e2b559abb262d1640c9
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 14:39:07 +0400

    Upadte PollVoteRepository

--- Repository/PollVoteRepository.java
+++ Repository/PollVoteRepository.java
@@ -2,6 +2,8 @@ package com.codeline.PollingApplication.Repository;
 
 import com.codeline.PollingApplication.Models.PollVote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PollVoteRepository extends JpaRepository<PollVote, Long> {
 }
