0100b7cf366cafcf1e1e580ae6a5be3bf1df5ed7
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 16:02:00 +0400

    Upadte PollVoteController

--- Controller/PollVoteController.java
+++ Controller/PollVoteController.java
@@ -2,20 +2,20 @@ package com.codeline.PollingApplication.Controller;
 
 
 import com.codeline.PollingApplication.Models.PollVote;
+import com.codeline.PollingApplication.Repository.PollVoteRepository;
 import com.codeline.PollingApplication.RequestObjects.GetPollVoteRequestObject;
 import com.codeline.PollingApplication.Service.PollVoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 @RestController
 public class PollVoteController {
     @Autowired
     PollVoteService pollVoteService;
-
+    PollVoteRepository pollVoteRepository;
     @RequestMapping("/api/polls/vote")
     public void savePollCreation(@RequestBody GetPollVoteRequestObject pollVoteRequestObject){
         createPollVote(pollVoteRequestObject);
@@ -26,7 +26,10 @@ public class PollVoteController {
     public List<PollVote> getPollVote(){
         return pollVoteService.getPollVote();}
 
-
+    @GetMapping("/count/{Choice}")
+    public long countOccurrences(@PathVariable("Choice") String choice) {
+        return pollVoteService.countOccurrences(choice);
+    }
     public void createPollVote(GetPollVoteRequestObject pollVoteRequestObject){
         PollVote pollVote= new PollVote();
         pollVote.setChoice(pollVoteRequestObject.getChoice());
