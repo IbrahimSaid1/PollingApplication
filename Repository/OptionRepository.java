6c72d8ec4d3155a5c1af877f838669e281704cfd
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 14:52:20 +0400

    Upadte GetPollVoteRequestObject

--- Repository/OptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codeline.PollingApplication.Repository;
-
-import com.codeline.PollingApplication.Models.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface OptionRepository extends JpaRepository<Option, Long> {
-}
