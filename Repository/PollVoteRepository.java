3107437e8f9ef48050231d1d58d3a192c2ecd867
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 16:02:18 +0400

    Upadte PollVoteRepository

--- Repository/PollVoteRepository.java
+++ Repository/PollVoteRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PollVoteRepository extends JpaRepository<PollVote, Long> {
+    long countByChoice(String choice);
 }
