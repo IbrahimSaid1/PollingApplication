4dfeff12bc8bd821cc6058e2244e4dbd42b7eaca
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 10:39:21 +0400

    Update PollCreationController

--- Controller/PollCreationController.java
+++ Controller/PollCreationController.java
@@ -1,7 +1,12 @@
 package com.codeline.PollingApplication.Controller;
 
+import com.codeline.PollingApplication.Models.Option;
 import com.codeline.PollingApplication.Models.PollCreation;
+import com.codeline.PollingApplication.Models.Register;
 import com.codeline.PollingApplication.Repository.PollCreationRepository;
+import com.codeline.PollingApplication.RequestObjects.GetPollCreationRequestObject;
+import com.codeline.PollingApplication.RequestObjects.GetRegisterRequestObject;
+import com.codeline.PollingApplication.Service.PollCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -17,6 +24,7 @@ public class PollCreationController{
 
     @Autowired
     PollCreationRepository pollCreationRepository;
+    PollCreationService pollCreationService;
 
     PollCreationController(PollCreationRepository pollCreationRepository){
         this.pollCreationRepository= pollCreationRepository;
@@ -42,4 +50,15 @@ public class PollCreationController{
         return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 
+
+    public void createPollCreation(GetPollCreationRequestObject pollCreationRequestObject){
+        PollCreation pollCreation= new PollCreation();
+        //pollCreation.setQuestion(pollCreationRequestObject.getQuestion());
+        pollCreation.setQuestion("What is your favorite color?");
+        pollCreation.setOption1("Red");
+        pollCreation.setOption2("Blue");
+        pollCreation.setOption3("Green");
+        pollCreationService.savePollCreation(pollCreation);
+
+    }
 }
