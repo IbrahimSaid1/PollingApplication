90883cbc08fbcd5e35da1287e04a0d0115a3b1ed
IbrahimSaid1 <squ118977@gmail.com>
2023-05-25 11:39:40 +0400

    Update PollCreationController

--- Controller/PollCreationController.java
+++ Controller/PollCreationController.java
@@ -2,21 +2,12 @@ package com.codeline.PollingApplication.Controller;
 
 import com.codeline.PollingApplication.Models.Option;
 import com.codeline.PollingApplication.Models.PollCreation;
-import com.codeline.PollingApplication.Models.Register;
-import com.codeline.PollingApplication.Repository.PollCreationRepository;
 import com.codeline.PollingApplication.RequestObjects.GetPollCreationRequestObject;
-import com.codeline.PollingApplication.RequestObjects.GetRegisterRequestObject;
 import com.codeline.PollingApplication.Service.PollCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
@@ -39,9 +30,7 @@ public class PollCreationController{
     public void createPollCreation(GetPollCreationRequestObject pollCreationRequestObject){
         PollCreation pollCreation= new PollCreation();
         pollCreation.setQuestion(pollCreationRequestObject.getQuestion());
-        pollCreation.setOption1(pollCreationRequestObject.getOption1());
-        pollCreation.setOption2(pollCreationRequestObject.getOption2());
-        pollCreation.setOption3(pollCreationRequestObject.getOption3());
+        pollCreation.setOptions(pollCreationRequestObject.getOptions());
         pollCreationService.savePollCreation(pollCreation);
 
     }
